01e9cf8d64e814a59559561f26e983fc906b6705
chylex <contact@chylex.com>
2020-08-14 19:10:43 +0200

    Fix formatting in forms.js & add old browser innerText workaround

--- res/~resources/js/forms.js
+++ res/~resources/js/forms.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
         
         let ignoreMultiselect = target.closest("details.multiselect");
         
-        if (ignoreMultiselect === null && target.tagName === 'LABEL'){
+        if (ignoreMultiselect === null && target.tagName === "LABEL"){
             ignoreMultiselect = target.nextElementSibling;
         }
         
@@ -19,12 +19,19 @@ document.addEventListener("DOMContentLoaded", function(){
     for(/** @type HTMLDetailsElement */ const multiselect of document.querySelectorAll("details.multiselect")){
         const label = multiselect.previousElementSibling;
         
-        label.addEventListener('click', function(){
+        label.addEventListener("click", function(){
             multiselect.open = !multiselect.open;
         });
         
+        /**
+         * @param {HTMLElement | Element} ele
+         */
+        function getText(ele){
+            return ele.innerText || ele.textContent; // fallback for buggy browsers
+        }
+        
         const summary = multiselect.querySelector("summary");
-        const summaryInitialText = summary.innerText;
+        const summaryInitialText = getText(summary);
         
         const checkboxes = multiselect.getElementsByTagName("input");
         
@@ -34,36 +41,36 @@ document.addEventListener("DOMContentLoaded", function(){
             for(/** @type HTMLInputElement */ const cb of checkboxes){
                 if (cb.checked){
                     // noinspection JSUnresolvedVariable
-                    checked.push(cb.nextElementSibling.innerText.trim());
+                    checked.push(getText(cb.nextElementSibling).trim());
                 }
             }
             
             if (checked.length === 0){
                 summary.innerText = summaryInitialText;
-                summary.style.fontStyle = 'italic';
+                summary.style.fontStyle = "italic";
             }
             else{
                 summary.innerText = checked.join(", ");
-                summary.style.fontStyle = 'normal';
+                summary.style.fontStyle = "normal";
             }
         };
         
         const updateAll = function(){
             setTimeout(function(){
                 for(/** @type HTMLInputElement */ const checkbox of checkboxes){
-                    checkbox.dispatchEvent(new Event('change'));
+                    checkbox.dispatchEvent(new Event("change"));
                 }
             }, 0);
         };
         
         for(/** @type HTMLInputElement */ const checkbox of checkboxes){
             checkbox.addEventListener("change", updateCallback);
-            checkbox.dispatchEvent(new Event('change'));
+            checkbox.dispatchEvent(new Event("change"));
             
             const form = checkbox.form;
             
             if (form !== null){
-                form.addEventListener('reset', updateAll);
+                form.addEventListener("reset", updateAll);
             }
         }
     }
