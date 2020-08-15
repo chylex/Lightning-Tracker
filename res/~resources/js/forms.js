6241b06ab8fb8f8250f33d558d8dd6ebf9862a45
chylex <contact@chylex.com>
2020-08-15 07:16:49 +0200

    Add form multiselect overflow slicing & fix excessive callbacks

--- res/~resources/js/forms.js
+++ res/~resources/js/forms.js
@@ -16,6 +16,10 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     });
     
+    // global multiselect text widths used for overflow slicing
+    const emptyPair = ["", 0];
+    const memoizedPairs = new Map();
+    
     for(/** @type HTMLDetailsElement */ const multiselect of document.querySelectorAll("details.multiselect")){
         const label = multiselect.previousElementSibling;
         
@@ -50,28 +54,92 @@ document.addEventListener("DOMContentLoaded", function(){
                 summary.style.fontStyle = "italic";
             }
             else{
+                if (summary.clientWidth > 0){
+                    const wasClosed = !multiselect.open;
+                    
+                    if (wasClosed){
+                        multiselect.open = true;
+                    }
+                    
+                    const testEle = document.createElement("summary");
+                    testEle.style.position = "absolute";
+                    testEle.style.overflow = "visible";
+                    summary.insertAdjacentElement("afterend", testEle);
+                    
+                    const targetWidth = summary.clientWidth - 7;
+                    
+                    function getTextWidth(text){
+                        testEle.innerText = text;
+                        return testEle.clientWidth;
+                    }
+                    
+                    while(getTextWidth(checked.join(", ")) >= targetWidth){
+                        const pairs = checked.map(str => {
+                            const sliced = Array.from(str).slice(0, -1).join("").trim(); // unicode
+                            
+                            if (sliced.length === 0){
+                                return emptyPair;
+                            }
+                            
+                            let pair = memoizedPairs.get(sliced);
+                            
+                            if (!pair){
+                                memoizedPairs.set(sliced, pair = [sliced, getTextWidth(sliced)]);
+                            }
+                            
+                            return pair;
+                        });
+                        
+                        let longestIndex = 0;
+                        
+                        for(let index = 1; index < pairs.length; index++){
+                            if (pairs[index][1] > pairs[longestIndex][1]){
+                                longestIndex = index;
+                            }
+                        }
+                        
+                        if (pairs[longestIndex] === emptyPair){
+                            break;
+                        }
+                        
+                        checked[longestIndex] = pairs[longestIndex][0];
+                    }
+                    
+                    testEle.remove();
+                    
+                    if (wasClosed){
+                        multiselect.open = false;
+                    }
+                }
+                
                 summary.innerText = checked.join(", ");
                 summary.style.fontStyle = "normal";
             }
         };
         
-        const updateAll = function(){
-            setTimeout(function(){
-                for(/** @type HTMLInputElement */ const checkbox of checkboxes){
-                    checkbox.dispatchEvent(new Event("change"));
-                }
-            }, 0);
+        const updateDelayed = function(){
+            setTimeout(updateCallback, 0);
         };
         
         for(/** @type HTMLInputElement */ const checkbox of checkboxes){
             checkbox.addEventListener("change", updateCallback);
-            checkbox.dispatchEvent(new Event("change"));
             
             const form = checkbox.form;
             
             if (form !== null){
-                form.addEventListener("reset", updateAll);
+                form.addEventListener("reset", updateDelayed);
             }
+        }
+        
+        updateCallback();
+        
+        if (summary.clientWidth === 0 && "IntersectionObserver" in window){
+            const observer = new IntersectionObserver(function(){
+                updateCallback();
+                observer.disconnect();
+            });
+            
+            observer.observe(summary);
         }
     }
     
