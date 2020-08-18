7352802a0314645eb7c9127964e6888371e239d9
chylex <contact@chylex.com>
2020-08-18 12:38:59 +0200

    Add optional placeholder to text fields

--- src/Pages/Components/Forms/Elements/FormTextField.php
+++ src/Pages/Components/Forms/Elements/FormTextField.php
@@ -10,6 +10,7 @@ final class FormTextField extends AbstractFormField{
   private string $id;
   private ?string $label;
   private string $type = 'text';
+  private ?string $placeholder = null;
   private ?string $autocomplete = null;
   
   public function __construct(string $id, string $name){
@@ -27,6 +28,11 @@ final class FormTextField extends AbstractFormField{
     return $this;
   }
   
+  public function placeholder(string $placeholder): self{
+    $this->placeholder = $placeholder;
+    return $this;
+  }
+  
   public function autocomplete(string $autocomplete): self{
     $this->autocomplete = $autocomplete;
     return $this;
@@ -37,6 +43,7 @@ final class FormTextField extends AbstractFormField{
     $label = $this->label ?? $name;
     $value = protect($this->value);
     
+    $placeholder_attr = $this->placeholder === null ? '' : ' placeholder="'.$this->placeholder.'"';
     $autocomplete_attr = $this->autocomplete === null ? '' : ' autocomplete="'.$this->autocomplete.'"';
     $disabled_attr = $this->disabled === false ? '' : ' disabled';
     $disabled_class = $this->disabled === false ? '' : ' class="disabled"';
@@ -44,7 +51,7 @@ final class FormTextField extends AbstractFormField{
     echo <<<HTML
 <div class="field-group">
   <label for="$this->id"$disabled_class>$label</label>
-  <input id="$this->id" name="$name" type="$this->type" value="$value"$autocomplete_attr$disabled_attr>
+  <input id="$this->id" name="$name" type="$this->type" value="$value"$placeholder_attr$autocomplete_attr$disabled_attr>
 HTML;
     
     $this->echoErrors();
